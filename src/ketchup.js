73330b1e3cea07e7d76b2581745d2d9219a36e37
Ladrillo <gabriel@cabrejas.net>
2021-11-30 13:44:57 +0100

    duh

--- src/ketchup.js
+++ src/ketchup.js
@@ -3,29 +3,30 @@ const { exec } = require('child_process')
 const chokidar = require('chokidar')
 const throttle = require('lodash.throttle')
 
-const branch = process.argv[2] || 'lecture'
+module.exports = function () {
+  const branch = process.argv[2] || 'lecture'
 
-const throttleConfig = {
-  leading: false,
-  trailing: true,
-}
+  const throttleConfig = {
+    leading: false,
+    trailing: true,
+  }
 
-const chokidarConfig = {
-  ignoreInitial: true,
-  ignored: ['**/node_modules/**/*', '**/.git/**/*'],
-}
+  const chokidarConfig = {
+    ignoreInitial: true,
+    ignored: ['**/node_modules/**/*', '**/.git/**/*'],
+  }
 
-const log = process => {
-  process.stdout.on('data', data => {
-    console.log(data)
-  })
-  process.stderr.on('data', data => {
-    console.error(data)
-  })
-}
+  const log = process => {
+    process.stdout.on('data', data => {
+      console.log(data)
+    })
+    process.stderr.on('data', data => {
+      console.error(data)
+    })
+  }
 
-const prep = () => {
-  const childProcess = exec(`
+  const prep = () => {
+    const childProcess = exec(`
     git checkout main
     git pull
     git branch -D ${branch}
@@ -33,20 +34,21 @@ const prep = () => {
     git checkout -b ${branch}
     git push origin ${branch}
   `)
-  log(childProcess)
-}
+    log(childProcess)
+  }
 
-const push = (event, path) => {
-  console.log(event, path)
-  const childProcess = exec(`
+  const push = (event, path) => {
+    console.log(event, path)
+    const childProcess = exec(`
     git add .
     git commit -m "committing to ${branch}"
     git push origin ${branch}
   `)
-  log(childProcess)
+    log(childProcess)
+  }
+
+  prep()
+
+  const throttledPush = throttle(push, 5000, throttleConfig)
+  chokidar.watch('.', chokidarConfig).on('all', throttledPush)
 }
-
-prep()
-
-const throttledPush = throttle(push, 5000, throttleConfig)
-chokidar.watch('.', chokidarConfig).on('all', throttledPush)
