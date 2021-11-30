6b9a83bb048b0baa57660c87813ad3b342313574
Ladrillo <gabriel@cabrejas.net>
2021-11-30 13:29:46 +0100

    clean

--- src/ketchup.js
+++ src/ketchup.js
@@ -1,15 +1,40 @@
 #!/usr/bin/env node
+const { exec } = require('child_process')
 const chokidar = require('chokidar')
+const throttle = require('lodash.throttle')
 
-const config = {
+const chokidarConfig = {
   ignoreInitial: true,
   ignored: ['**/node_modules/**/*', '**/.git/**/*'],
 }
 
+const throttleConfig = {
+  leading: false,
+  trailing: true,
+}
+
+const push = (event, path) => {
+  console.log(event, path)
+  const branch = process.argv[2] || 'lecture'
+  const childProcess = exec(`
+    git stash
+    git checkout main
+    git pull
+    git branch -D ${branch}
+    git push origin :${branch}
+    git checkout -b ${branch}
+    git push origin ${branch}
+  `)
+  childProcess.stdout.on('data', (data) => {
+    console.log(data)
+  })
+  childProcess.stderr.on('data', (data) => {
+    console.error(data)
+  })
+}
+
+const throttledPush = throttle(push, 5000, throttleConfig)
+
 module.exports = function () {
-  chokidar
-    .watch('.', config)
-    .on('all', (event, path) => {
-      console.log(event, path)
-    })
+  chokidar.watch('.', chokidarConfig).on('all', throttledPush)
 }
